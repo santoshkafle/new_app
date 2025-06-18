36386ba8b4fa8e5ece162a7fd861d1bbea9655d8
santosh kafle <santoshkafle156@gmail.com>
2025-06-18 13:11:49 +0545

    gridview with search mec

--- lib/pageview/gridview_page.dart
+++ lib/pageview/gridview_page.dart
@@ -33,36 +33,92 @@ class _GridviewPageState extends State<GridviewPage> {
     Fruitmodel(name: 'Mango', price: '170', imageUrl: Fruitpath.mangoUrl),
     Fruitmodel(name: 'Lichi', price: '250', imageUrl: Fruitpath.lichiUrl),
   ];
+  TextEditingController searchController = TextEditingController();
+  List<Fruitmodel> filteredList = [];
+
+  @override
+  void initState() {
+    super.initState();
+
+    filteredList = fruitList;
+
+    searchController.addListener(() {
+      searchFruit(searchController.text);
+    });
+  }
+
+  void searchFruit(String searchIteam) {
+    final result =
+        fruitList
+            .where(
+              (fruit) =>
+                  fruit.name.toLowerCase().contains(
+                    searchIteam.toLowerCase(),
+                  ) ||
+                  fruit.price.contains(searchIteam),
+            )
+            .toList();
+
+    setState(() {
+      filteredList = result;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Center(child: Text("Fruit List:"))),
-      body: GridView.builder(
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 3,
-          mainAxisSpacing: 30,
-          crossAxisSpacing: 20,
-        ),
-        itemCount: fruitList.length,
-        padding: EdgeInsets.all(20),
-        itemBuilder: (context, index) {
-          return Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              color: Colors.grey[300],
+      body: Column(
+        children: [
+          //serch bar...
+          Padding(
+            padding: const EdgeInsets.all(15),
+            child: TextField(
+              controller: searchController,
+              decoration: InputDecoration(
+                hintText: "Search fruit:",
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                prefixIcon: Icon(Icons.search),
+              ),
             ),
-            padding: EdgeInsets.all(5),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Image.asset(fruitList[index].imageUrl, height: 50, width: 50),
-                Text(fruitList[index].name),
-                Text("Rs: ${fruitList[index].price}"),
-              ],
+          ),
+          //food iteams
+          Expanded(
+            child: GridView.builder(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3,
+                mainAxisSpacing: 30,
+                crossAxisSpacing: 20,
+              ),
+              itemCount: filteredList.length,
+              padding: EdgeInsets.all(20),
+              itemBuilder: (context, index) {
+                return Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.grey[300],
+                  ),
+                  padding: EdgeInsets.all(5),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Image.asset(
+                        filteredList[index].imageUrl,
+                        height: 50,
+                        width: 50,
+                      ),
+                      Text(filteredList[index].name),
+                      Text("Rs: ${filteredList[index].price}"),
+                    ],
+                  ),
+                );
+              },
             ),
-          );
-        },
+          ),
+        ],
       ),
     );
   }
