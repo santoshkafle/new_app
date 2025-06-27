c23981ab47d069884e4bb945e6def82820bcd981
santosh kafle <santoshkafle156@gmail.com>
2025-06-27 12:39:10 +0545

    hoamepage on progress

--- lib/pageview/homepage.dart
+++ lib/pageview/homepage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:new_app/utils/fruit_list.dart';
+import 'package:new_app/view/fruit_details_page.dart';
 
 class Homepage extends StatelessWidget {
   const Homepage({super.key});
@@ -6,8 +8,145 @@ class Homepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Center(child: Text("HomePage!"))),
-      body: Center(child: Text("This is a Homepage.")),
+      appBar: AppBar(
+        title: Text("eGrocery", style: TextStyle(fontSize: 20)),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          spacing: 12,
+          children: [
+            Container(
+              padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+              decoration: BoxDecoration(
+                color: Color(0xffd6eedb),
+                borderRadius: BorderRadius.circular(20),
+              ),
+
+              height: 200,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Fresh Fruits',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Spacer(),
+                      Text(
+                        'Upto',
+                        style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          Text(
+                            '20',
+                            style: TextStyle(
+                              fontSize: 32,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          SizedBox(width: 4),
+                          Text(
+                            '%',
+                            style: TextStyle(
+                              fontSize: 26,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          SizedBox(width: 4),
+                          Text(
+                            'Discount',
+                            style: TextStyle(
+                              fontSize: 22,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  Spacer(),
+                  Image.asset(
+                    'assest/banner/banner_fruits.jpeg',
+                    height: 100,
+                    width: 100,
+                  ),
+                ],
+              ),
+            ),
+            // list here,,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Fresh Fruit",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+                ),
+                TextButton(onPressed: () {}, child: Text("See More")),
+              ],
+            ),
+            GridView.builder(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                mainAxisSpacing: 30,
+                crossAxisSpacing: 20,
+              ),
+              itemCount: FruitList.fruitList.length,
+              padding: EdgeInsets.all(20),
+              //   scrollDirection: Axis.horizontal,
+              physics: NeverScrollableScrollPhysics(),
+              itemBuilder: (context, index) {
+                return InkWell(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder:
+                            (context) => FruitDetailsPage(
+                              fruit: FruitList.fruitList[index],
+                            ),
+                      ),
+                    );
+                  },
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.grey[300],
+                    ),
+                    padding: EdgeInsets.all(5),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Image.asset(
+                          FruitList.fruitList[index].imageUrl,
+                          height: 50,
+                          width: 50,
+                        ),
+                        Text(FruitList.fruitList[index].name),
+                        Text("Rs: ${FruitList.fruitList[index].price}"),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
